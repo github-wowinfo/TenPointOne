9260c5738d1502059749c08d08dcae71eb2d54f9
Husainwow <husain.f@wowinfobiz.com>
2021-11-30 17:54:18 +0530

    Sega Creation

---
+++ src/utility/hooks/useVaults.js
@@ -0,0 +1,131 @@
+import { TransactionStatus, useContractCall, useContractFunction } from "@usedapp/core"
+import { utils, constants } from "ethers"
+import { Contract } from "@ethersproject/contracts"
+import { useState, useEffect } from "react"
+
+import Vault from '../../brownie_exports/chain-info/contracts/Vault.json'
+// import Sega from '../../brownie_exports/chain-info/contracts/SEGA.json'
+
+export const useVault = (vault) => {
+    //console.log("In useVault - Vault:", vault)
+
+    //const SegaInterface = new utils.Interface(Sega.abi)
+    const VaultInterface = new utils.Interface(Vault.abi)
+    const VaultContract = new Contract(vault ? vault : constants.AddressZero, VaultInterface)
+
+    /*-------------------------------------------------------
+    -----------------Blue View Only Functions----------------
+    -------------------------------------------------------*/
+
+    // getRecoveryInfo() - Get Recovery Info
+    const getRecoveryInfoFn = { abi: VaultInterface, address: vault, method: "getRecoveryInfo", args: [], }
+    const [_owner, _backup, c, d] = useContractCall(getRecoveryInfoFn) ?? []
+    const getRecoveryInfo = () => {
+        const x = new Date(c.toNumber() * 1000)
+        const _releaseDt = x.toLocaleDateString('en-Gbackup', { day: 'numeric', month: 'short', year: 'numeric' }).replace(/ /g, ' ')
+        const _unlockDays = d.toNumber()
+        console.log("Original Info")
+        console.log(_owner, _backup, c, d)
+        console.log("Rec Info:", _owner, _backup, _releaseDt, _unlockDays)
+        return { _owner, _backup, _releaseDt, _unlockDays }
+    }
+
+    // viewSegaList() - Get list of Segas linked to this Vault
+    const getSegaListFn = { abi: VaultInterface, address: vault, method: "viewSegaList", args: [], }
+    const [segaList] = useContractCall(getSegaListFn) ?? []
+    const getSegaList = () => {
+        console.log("Sega List:", { segaList })
+        return segaList
+    }
+
+    /*-------------------------------------------------------
+    ------------------Red Vault Security Fn------------------
+    -------------------------------------------------------*/
+
+    // setBackup() - To set a new backup account
+    const { send: changeBackupSend, state: changeBackupState } =
+        useContractFunction(VaultContract, "setBackup", { transactionName: "Set Backup Account" })
+    const changeBackup = (address) => { return changeBackupSend(address) }
+
+    // setUnlockPeriod() - To set a new inactivity time period
+    const { send: changeUnlockPeriodSend, state: changeUnlockPeriodState } =
+        useContractFunction(VaultContract, "setUnlockPeriod", { transactionName: "Set Unlock Period" })
+    const changeUnlockPeriod = (days) => { return changeUnlockPeriodSend(days) }
+
+    // claimVault() - For the backup account to claim ownership
+    const { send: claimVaultSend, state: claimVaultState } =
+        useContractFunction(VaultContract, "claimVault", { transactionName: "Claim Vault" })
+    const claimVault = () => { return claimVaultSend() }
+
+    // createNewSega() - To Launch a new SEGA
+    const { send: createNewSegaSend, state: createNewSegaState } =
+        useContractFunction(VaultContract, "createNewSega", { transactionName: "Create New Sega" })
+    const createNewSega = () => { return createNewSegaSend() }
+
+    const [txnState, setTxnState] = useState(changeBackupState)
+    const testSameTxn = (a, b) => {
+        return (a.transaction?.hash === b.transaction?.hash)
+    }
+    useEffect(() => {
+        console.log("Txn State Before: ", txnState.transaction?.hash, txnState.status)
+
+        if (changeBackupState.status === "Mining") {
+            setTxnState(changeBackupState)
+        } else if (changeUnlockPeriodState.status === "Mining") {
+            setTxnState(changeUnlockPeriodState)
+        } else if (claimVaultState.status === "Mining") {
+            setTxnState(claimVaultState)
+        } else if (testSameTxn(txnState, changeBackupState) && changeBackupState.status === "Success") {
+            setTxnState(changeBackupState)
+        } else if (testSameTxn(txnState, changeUnlockPeriodState) && changeUnlockPeriodState.status === "Success") {
+            setTxnState(changeUnlockPeriodState)
+        } else if (testSameTxn(txnState, claimVaultState) && claimVaultState.status === "Success") {
+            setTxnState(claimVaultState)
+        } else { console.log("Not Mining") }
+
+        console.log("Txn State After: ", txnState.transaction?.hash, txnState.status)
+    }, [changeBackupState, changeUnlockPeriodState, claimVaultState, txnState])
+
+    /*------------------------------------------------------
+    ------------------Sega Management Fn--------------------
+    -------------------------------------------------------*/
+    /*
+        // getInfo() - To Freeze the SEGA's Trader
+        const getSegaInfoFn = {abi:SegaInterface, address:sega.toString(), method:"getInfo", args:[],}
+        const [_vault,_trader,_active]: any = useContractCall(getSegaInfoFn) ?? []
+        const getSegaInfo = () => {
+            if (sega.length>0) {
+                console.log("Rec Info:",_vault,_trader,_active)
+                return {_vault,_trader,_active}
+            }
+        }
+    
+        // pauseSega() - To Freeze the SEGA's Trader
+        const {send: xxSend, state: xxState} =
+        useContractFunction(VaultContract,"xx", { transactionName:"xx" } )
+        const xx = (address:string) => { return xxSend(address) }
+    
+        // unpauseSega() - To Unfreeze the SEGA's Trader
+        const {send: xxSend, state: xxState} =
+        useContractFunction(VaultContract,"xx", { transactionName:"xx" } )
+        const xx = (address:string) => { return xxSend(address) }
+        
+        // changeTrader() - To Freeze the SEGA's Trader
+        const {send: xxSend, state: xxState} =
+        useContractFunction(VaultContract,"xx", { transactionName:"xx" } )
+        const xx = (address:string) => { return xxSend(address) }
+    */
+
+    return {
+        getRecoveryInfo,
+        getSegaList,
+        changeBackup,
+        changeUnlockPeriod,
+        claimVault,
+        txnState,
+        createNewSega,
+        createNewSegaState,
+    }
+
+
+}
--- src/views/Manager/Create/CreateSegaModal.js
+++ src/views/Manager/Create/CreateSegaModal.js
@@ -1,64 +1,166 @@
 import Select from 'react-select'
 import { X, PlusCircle } from 'react-feather'
 import { selectThemeColors } from '@utils'
-import {Modal, ModalBody, ModalHeader, ModalFooter, Row, Col, Input, Label, FormGroup, Button} from 'reactstrap'
+import { Modal, ModalBody, ModalHeader, ModalFooter, Row, Col, Input, Label, FormGroup, Button, Alert, Spinner } from 'reactstrap'
+import { getAddress, hexStripZeros } from "ethers/lib/utils"
+import { useEthers, getExplorerAddressLink, getExplorerTransactionLink, shortenIfAddress, shortenIfTransactionHash } from "@usedapp/core"
+import React, { useState, useEffect } from "react"
+import { useRCU } from '../../../utility/hooks/useRCU'
+import { useVault } from '../../../utility/hooks/useVaults'
 
-const CreateSegaModal = ({ opensega, handleSegaModal}) => {
+const CreateSegaModal = ({ opensega, handleSegaModal }) => {
+
+    const { chainId } = useEthers()
+
+    const { getVaultList } = useRCU()
+    const [VaultList, setVaultList] = useState([])
+    const handleGetAllVaults = () => {
+        const x = getVaultList()
+        setVaultList(x)
+        console.log("Vault-List : ", x)
+    }
+
+    // Declaring State Hooks
+    const [SegaList, setSegaList] = useState([])
+    const [haveInfo, setHaveInfo] = useState(0)
+
+    // Initialise Vault to Manage
+    const [Vault, setVault] = useState("")
+    const handleSetVault = (value) => {
+        console.log(value.label)
+        setSegaList([])
+        setHaveInfo(0)
+        setVault(value.label)
+    }
+
+    // Import neccesary functions from useVaults.ts
+    const { getRecoveryInfo,
+        getSegaList,
+        changeBackup,
+        changeUnlockPeriod,
+        claimVault,
+        txnState,
+        createNewSega,
+        createNewSegaState,
+    } = useVault(Vault.toString())
+
+    //GET LIST OF VAULT's SEGAs
+    const handleGetSegas = () => {
+        if (Vault.length > 0) {
+            const x = getSegaList()
+            setSegaList(x)
+            console.log("Sega-List : ", x)
+        } else {
+            handleGetAllVaults()
+        }
+    }
+
+    //SNACKBAR Settings for Sega Launches
+    const [launchSegaTxn, setLaunchSegaTxn] = useState("")
+    const [newSegaAddress, setNewSegaAddress] = useState("")
+    const [showSegaLaunchingSnack, setShowSegaLaunchingSnack] = useState(false)
+    const [showSegaCreatedSnack, setShowSegaCreatedSnack] = useState(false)
+    const handleSnackClose = () => {
+        console.log("Snack Close - Sega Creation Txn:", getExplorerTransactionLink(launchSegaTxn, Number(chainId)))
+        console.log("Snack Close - New Sega Created:", getExplorerAddressLink(newSegaAddress, Number(chainId)))
+        setShowSegaLaunchingSnack(false)
+        setShowSegaCreatedSnack(false)
+    }
+
+    // Launch Sega Button Handles
+    const isLaunchInProgress = (createNewSegaState.status === "Mining")
+    const handleLaunchSega = () => {
+        console.log("Trying to Launch Sega :")
+        return createNewSega()
+    }
+    // To track state of Sega creation Trasnactions
+    useEffect(() => {
+        if (createNewSegaState.status === "Mining") {
+            const tx_id = createNewSegaState.transaction?.hash
+            setLaunchSegaTxn(tx_id)
+            console.log("***Handle TX_ID: ", shortenIfTransactionHash(tx_id))
+            setShowSegaLaunchingSnack(true)
+        }
+        if (createNewSegaState.status === "Success") {
+            const newSega = getAddress(hexStripZeros(createNewSegaState.receipt?.logs[1].topics[2]))
+            setNewSegaAddress(newSega)
+            console.log("***Handle New Sega: ", newSega)
+            console.log("***Handle Short Sega: ", shortenIfAddress(newSega))
+            setShowSegaCreatedSnack(true)
+        }
+    }, [createNewSegaState])
 
     // const CloseBtn = <X className='cursor-pointer' size={25} onClick={handleModal} />
 
-    const colourOptions = [
-        { value: 'adrs', label: 'Address 1' },
-        { value: 'adrs', label: 'Address 2' },
-        { value: 'adrs', label: 'Address 3' },
-        { value: 'adrs', label: 'Address 4' },
-        { value: 'adrs', label: 'Address 5' }
-      ]
+    const vlist = VaultList.map((vault, index) => ({ value: index, label: vault }))
 
     return (
-        <Modal className='modal-dialog-centered' isOpen={opensega} toggle={handleSegaModal} >
-                <ModalHeader tag='h2' toggle={handleSegaModal}>
-                    New Sega
-                </ModalHeader>
-                <ModalBody style={{ display: 'flex', flexDirection: 'column', justifyContent:'center', alignItems:'center'}}>
-                    <Row style={{ display: 'flex', flexDirection: 'column', justifyContent:'center', alignItems:'center'}}>
-                        <Col>
-                            <h3>Create a new independent Sega linked to any of your Vault.</h3>
-                        </Col>
-                        <Col>
-                            <p>All names set by you are stored locally on your PC and are not collected by Risk Protocol. Current user will also be designated as the owner 
-                                for the new Vault.
-                            </p>
-                        </Col>
-                        <Col>
-                            <p>You will be required to pay the network fees for new Vault creation.</p>
-                        </Col>
-                        <Col className='mb-1'>
-                            <Label style={{fontSize: "1.3em"}}>Parent Vault</Label>
-                            <Select
-                            // theme={selectThemeColors}
+        <Modal className='modal-dialog-centered modal-lg' isOpen={opensega} toggle={handleSegaModal} >
+            <ModalHeader tag='h2' toggle={handleSegaModal}>
+                New Sega
+            </ModalHeader>
+            <ModalBody style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                <Row style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                    <Col>
+                        <h3>Create a new independent Sega linked to any of your Vault.</h3>
+                    </Col>
+                    <Col>
+                        <p>All names set by you are stored locally on your PC and are not collected by Risk Protocol. Current user will also be designated as the owner
+                            for the new Vault.
+                        </p>
+                    </Col>
+                    <Col>
+                        <p>You will be required to pay the network fees for new Vault creation.</p>
+                    </Col>
+                    <Col className='mb-1'>
+                        <div className='d-flex flex-row justify-content-between my-1'>
+                            <Label style={{ fontSize: "1.3em" }}>Parent Vault</Label>
+                            <Button.Ripple size='sm' color='primary' onClick={handleGetAllVaults}>Refresh</Button.Ripple>
+                        </div>
+                        <Select
                             className='react-select'
                             classNamePrefix='select'
                             defaultValue=''
                             name='clear'
-                            options={colourOptions}
-                            isClearable
-                            />
-                        </Col>
-                        <Col>
-                            <FormGroup>
-                                <Label for='nickname' style={{fontSize: "1.3em"}}>Nickname</Label>
-                                <Input type='text' id='nickname' />
-                            </FormGroup>
-                        </Col>
-                    </Row>
-                </ModalBody>
-                <ModalFooter className='justify-content-center'>
-                    <Button.Ripple color='primary'  onClick={handleSegaModal}>
-                        <PlusCircle className='mr-1' size={25}/>
-                            Create
-                    </Button.Ripple>
-                </ModalFooter>
+                            options={vlist}
+                            onChange={handleSetVault}
+                        />
+                    </Col>
+                    <Col>
+                        <FormGroup>
+                            <Label for='nickname' style={{ fontSize: "1.3em" }}>Nickname</Label>
+                            <Input type='text' id='nickname' />
+                        </FormGroup>
+                    </Col>
+                    <Col>
+                        <Button.Ripple className='mx-1' onClick={handleGetSegas}>Show all SEGA's</Button.Ripple>
+                        <span>
+                            {SegaList.length > 0 ? (`${SegaList.length - 1} SEGAs - see console`) : "Get List of All SEGAs"}
+                        </span>
+                    </Col>
+                </Row>
+            </ModalBody>
+            <ModalFooter className='justify-content-center'>
+                <Button.Ripple color='primary' id='controlledPopover' onClick={handleLaunchSega}>
+                    {isLaunchInProgress ? <Spinner color='light' size='sm' /> : <span><PlusCircle className='mr-1' size={17} />Create</span>}
+                </Button.Ripple>
+                <div>
+                    <Alert isOpen={showSegaLaunchingSnack} toggle={() => handleSnackClose()} color="info">
+                        <div>Sega Launch in Progress. Transaction ID : &emsp; </div>
+                        <a href={getExplorerTransactionLink(launchSegaTxn, chainId ? chainId : 1)}
+                            target="_blank" rel="noreferrer">
+                            {launchSegaTxn} </a>
+                    </Alert>
+                    <Alert isOpen={showSegaCreatedSnack} toggle={() => handleSnackClose()} color="success">
+                        New Sega Launched :
+                        <div><a href={getExplorerAddressLink(newSegaAddress, chainId ? chainId : 1)} target="_blank" rel="noreferrer">
+                            {newSegaAddress} </a> </div>
+                        <div>Transaction ID :</div>
+                        <a href={getExplorerTransactionLink(launchSegaTxn, chainId ? chainId : 1)}
+                            target="_blank" rel="noreferrer"> {launchSegaTxn} </a>
+                    </Alert>
+                </div>
+            </ModalFooter>
         </Modal>
     )
 }
