2c4842c7716eab89948a91df580bd9b2cf8fe721
Husainwow <husain.f@wowinfobiz.com>
2021-12-06 12:01:05 +0530

    added new files and segalist population

--- src/utility/hooks/useRCU.js
+++ src/utility/hooks/useRCU.js
@@ -1,28 +1,89 @@
 import { useEthers, useContractFunction, useContractCall } from "@usedapp/core"
 import { utils, constants } from "ethers"
+import { getAddress } from "ethers/lib/utils"
 import { Contract } from "@ethersproject/contracts"
-import brownieConfig from '../../brownie_exports/brownie-config.json'
-import helperConfig from '../../helper-config.json'
-import RCU from '../../brownie_exports/chain-info/contracts/DeployRCU_P.json'
+import brownieConfig from "../../brownie_exports/brownie-config.json"
+import helperConfig from "../../helper-config.json"
+
+import RCU from "../../brownie_exports/chain-info/contracts/DeployRCU_P.json"
+import vFact from "../../brownie_exports/chain-info/contracts/VaultFactP.json"
+import sFact from "../../brownie_exports/chain-info/contracts/SegaFactP.json"
 
 export const useRCU = () => {
 
     const { chainId } = useEthers()
     const networkName = chainId ? helperConfig[chainId] : "Not Connected"
     const rcu = chainId ? brownieConfig["networks"][networkName]["RCU"] : constants.AddressZero
+    const _sf = chainId ? brownieConfig["networks"][networkName]["sFact"] : constants.AddressZero
+    const _vf = chainId ? brownieConfig["networks"][networkName]["vFact"] : constants.AddressZero
 
-    const { abi } = RCU
-    const RcuInterface = new utils.Interface(abi)
+    const RcuInterface = new utils.Interface(RCU.abi)
     const RcuContract = new Contract(rcu, RcuInterface)
+    const vFactInterface = new utils.Interface(vFact.abi)
+    const sFactInterface = new utils.Interface(sFact.abi)
+
+
+    //RiskProtocol() - get Admin
+    const RiskProtoFunction = { abi: RcuInterface, address: rcu, method: "RiskProtocol", args: [], }
+    const [RPAdmin] = useContractCall(rcu && RiskProtoFunction) ?? []
+    const getRPAdmin = () => {
+        console.log("RP Admin :", { RPAdmin })
+        return getAddress(RPAdmin)
+    }
+
+    //VaultFact() - get Currecnt Vault Factory in Use
+    // There is some issue here - Refresh happens on second click
+    // If i change VF to sega address and then revert, it gets stuck at sega address.
+    // Network instructions are going out fine. It is changing at the back end - but the front end gets stuck
+    // Refresh page solves it
+    const VF_Fn = { abi: RcuInterface, address: rcu, method: "VaultFact", args: [], }
+    const [VF] = useContractCall(rcu && VF_Fn) ?? []
+    const getVaultFactory = () => {
+        console.log("Vault Factory in TS file :", { VF })
+        return (VF ? VF : "")
+    }
+
+    //SegaFactory - get Currecnt Vault Factory in Use
+    const SF_Fn = { abi: vFactInterface, address: (VF ? VF : _vf), method: "segaFactory", args: [], }
+    const [SF] = useContractCall(VF && SF_Fn) ?? []
+    const getSegaFactory = () => {
+        console.log("Sega Factory in TS file :", { SF })
+        return (SF ? SF : "")
+    }
 
     // getVaultList() - SHOW ALL VAULTS
     const getVaultListFn = { abi: RcuInterface, address: rcu, method: "getVaultList", args: [], }
-    const [vaultList] = useContractCall(getVaultListFn) ?? []
+    const [vaultList] = useContractCall(rcu && getVaultListFn) ?? []
     const getVaultList = () => {
         console.log("All Vault List :", { vaultList })
         return vaultList
     }
 
+    // getSegaList() - SHOW ALL SEGAs
+    const getSegaListFn = { abi: sFactInterface, address: (SF ? SF : _sf), method: "getSegaList", args: [], }
+    const [segaList] = useContractCall(SF && getSegaListFn) ?? []
+    const getSegaList = () => {
+        console.log("All Sega List :", { segaList })
+        return segaList
+    }
+
+    // changeAdmin() - Admin Function Only
+    const { send: changeAdminSend, state: changeAdminState } =
+        useContractFunction(RcuContract, "changeAdmin", { transactionName: "Change RP Admin" })
+    const changeAdmin = (address) => { return changeAdminSend(address) }
+
+    // changeAdmin() - Admin Function Only
+    const { send: changeVFactSend, state: changeVFactState } =
+        useContractFunction(RcuContract, "changeVaultFactory", { transactionName: "Change VFact" })
+    const changeVFact = (address) => { return changeVFactSend(address) }
+
+    // changeAdmin() - Admin Function Only
+    const { send: changeSFactSend, state: changeSFactState } =
+        useContractFunction(RcuContract, "updateSegaFactList", { transactionName: "Update SFact" })
+    const changeSFact = (address, onoff) => { return changeSFactSend(address, onoff) }
+
+
+    // launchVault() - Public Function
     const { send: launchVaultSend, state: launchVaultState } =
         useContractFunction(RcuContract, "launchVault", { transactionName: "Create New Vault" })
     const launchVault = () => {
@@ -31,8 +92,18 @@ export const useRCU = () => {
     }
 
     return {
+        getRPAdmin,
+        getVaultFactory,
+        getSegaFactory,
+        getVaultList,
+        getSegaList,
+        changeAdmin,
+        changeAdminState,
+        changeVFact,
+        changeVFactState,
+        changeSFact,
+        changeSFactState,
         launchVault,
         launchVaultState,
-        getVaultList,
     }
 }
--- src/views/Manager/security/VaultSecurity/ChildrenSega.js
+++ src/views/Manager/security/VaultSecurity/ChildrenSega.js
@@ -5,7 +5,7 @@ import { Modal, ModalBody, ModalHeader, ModalFooter, Row, Col, Button, Table } f
 import DataTable from 'react-data-table-component'
 import '@styles/react/libs/tables/react-dataTable-component.scss'
 
-const ChildrenSega = ({ openchildsegamodal, handleChildSegatModal }) => {
+const ChildrenSega = ({ openchildsegamodal, handleChildSegatModal, vault, segas }) => {
 
     const columns = [
         {
@@ -17,41 +17,46 @@ const ChildrenSega = ({ openchildsegamodal, handleChildSegatModal }) => {
                     {row.icon2}
                 </div>
             )
-        },
-        {
-            name: 'Created On',
-            maxWidth: '130px',
-            // paddingLeft: '0px', 
-            selector: row => row.date
         }
+        // {
+        //     name: 'Created On',
+        //     maxWidth: '130px',
+        //     selector: row => row.date
+        // }
     ]
 
-    const data = [
-        {
-            id: '1',
-            address: '0x851De59419E68133803eC175159b9F3ecFf8C5A0',
-            icon1: <FaRegCopy className='mx-1' />,
-            icon2: <GoLinkExternal className='mx-1' />,
-            date: '12 Sep 2021'
-        },
-        {
-            id: '2',
-            address: '0x851De59419E68133803eC175159b9F3ecFf8C5A0',
-            icon1: <FaRegCopy className='mx-1' />,
-            icon2: <GoLinkExternal className='mx-1' />,
-            date: '01 Oct 2021'
-        },
-        {
-            id: '3',
-            address: '0x851De59419E68133803eC175159b9F3ecFf8C5A0',
-            icon1: <FaRegCopy className='mx-1' />,
-            icon2: <GoLinkExternal className='mx-1' />,
-            date: '10 Oct 2021'
-        }
-    ]
+    // const data = [
+    //     {
+    //         id: '1',
+    //         address: '0x851De59419E68133803eC175159b9F3ecFf8C5A0',
+    //         icon1: <FaRegCopy className='mx-1' />,
+    //         icon2: <GoLinkExternal className='mx-1' />,
+    //         date: '12 Sep 2021'
+    //     },
+    //     {
+    //         id: '2',
+    //         address: '0x851De59419E68133803eC175159b9F3ecFf8C5A0',
+    //         icon1: <FaRegCopy className='mx-1' />,
+    //         icon2: <GoLinkExternal className='mx-1' />,
+    //         date: '01 Oct 2021'
+    //     },
+    //     {
+    //         id: '3',
+    //         address: '0x851De59419E68133803eC175159b9F3ecFf8C5A0',
+    //         icon1: <FaRegCopy className='mx-1' />,
+    //         icon2: <GoLinkExternal className='mx-1' />,
+    //         date: '10 Oct 2021'
+    //     }
+    // ]
+
+    // const actualSegas = segas.filter(label => label !== '')
+
+    const data = segas
     // const CloseBtn = <X className='cursor-pointer' size={25} onClick={handleModal} />
     return (
         <Modal className='modal-dialog-centered modal-lg' isOpen={openchildsegamodal} toggle={handleChildSegatModal} >
+            {console.log('segas', segas)}
+            {console.log('data', data)}
             <ModalHeader tag='h1' toggle={handleChildSegatModal}>
                 Children Sega
             </ModalHeader>
@@ -65,7 +70,7 @@ const ChildrenSega = ({ openchildsegamodal, handleChildSegatModal }) => {
                             <Col md='1'><BsSafe2 size={40} /></Col>
                             <Col className='d-flex flex-column justify-content-start'>
                                 <h3>SBI Vault</h3>
-                                <h5>0x271AC3C918C8C49a1723058e82A985ad0599EFe2</h5>
+                                <h5>{vault}</h5>
                             </Col>
                         </Row>
                     </Col>
@@ -80,7 +85,6 @@ const ChildrenSega = ({ openchildsegamodal, handleChildSegatModal }) => {
                             selectableRows
                             noHeader
                         />
-
                     </Col>
                 </Row>
             </ModalBody>
