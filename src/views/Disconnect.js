e7495cccebc5f52c85583c65d38bd2bd7d6bd60e
Husainwow <husain.f@wowinfobiz.com>
2022-03-03 10:47:38 +0530

    disconnet href issue

--- src/views/Disconnect.js
+++ src/views/Disconnect.js
@@ -3,5 +3,5 @@ import { useEthers } from '@usedapp/core'
 export const Disconnect = () => {
     const { deactivate } = useEthers()
     deactivate()
-    window.location.href = '/home'
+    window.location.href = 'home'
 } 
