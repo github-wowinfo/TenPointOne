3be6ca0afc25d6f0db6cccabba1d24e64be02531
Husainwow <husain.f@wowinfobiz.com>
2022-03-31 23:41:31 +0530

    send adrs book bg color issue fix

--- src/views/Send/AdrsBookSelect.js
+++ src/views/Send/AdrsBookSelect.js
@@ -1,11 +1,13 @@
 import { ChainId, shortenIfAddress, useEthers } from "@usedapp/core"
+import { useEffect } from "react"
 import { connect } from "react-redux"
 import { Button, CardSubtitle, CardTitle, Col, Modal, ModalBody, ModalHeader, Row } from "reactstrap"
 import NonVandS from "./NonVandS"
 import VandS from "./VandS"
 
 const AdrsBookSelect = ({ openadrsbookselect, handleadrsbookModal, setAdrsBookValue }) => {
-
+    const color = JSON.parse(localStorage.getItem('skin'))
+    console.log('color', color)
     const bgstyle = {
         backgroundColor: '#000000',
         backgroundImage: 'linear-gradient(147deg, #000000 66%, #0f3070e8 94%)'
@@ -17,7 +19,7 @@ const AdrsBookSelect = ({ openadrsbookselect, handleadrsbookModal, setAdrsBookVa
                 <ModalHeader tag='h2' toggle={() => handleadrsbookModal()}>
                     Select Address
                 </ModalHeader>
-                <ModalBody style={bgstyle}>
+                <ModalBody style={color === 'dark' ? bgstyle : null}>
                     <Row className='d-flex flex-row justify-content-center'>
                         <Col md='6'><NonVandS handleadrsbookModal={handleadrsbookModal} setAdrsBookValue={setAdrsBookValue} /></Col>
                         <Col md='6'><VandS handleadrsbookModal={handleadrsbookModal} setAdrsBookValue={setAdrsBookValue} /></Col>
